78963ca862456b9c0d28ecfe7a7d267feef1b045
Cecil Lam <cglam@ucdavis.edu>
2017-05-23 02:19:11 -0700

    added date, (de)select all, remove selected

--- tracker.js
+++ tracker.js
@@ -1,10 +1,12 @@
 angular.module('route').controller('tracker', function($scope) {
 
+// VARIABLE DECLARATIONS
   $scope.currencies = ['$', 'euro', 'yen'];
   $scope.categories = ['Food', 'Housing', 'Transportation']
-
+  $scope.ordering =
 
   $scope.transactions = [{
+    date : new Date(),
     selected : true,
     currency : '$',
     amount : 1,
@@ -12,10 +14,16 @@ angular.module('route').controller('tracker', function($scope) {
     category : 'General'
   }];
 
+  $scope.deletedTransactions = [];
+
+// FUNCTION DECLARATIONS
+
+// PUSHES TRANSACTIONS TO RUNNING TOTAL LIST OF TRANSACTIONS
   $scope.addTransaction = function(curType, amt, desc, cat){
     $scope.amount = '';
     $scope.description = '';
     $scope.transactions.push({
+        date : new Date(),
         selected : false,
         currency : curType,
         amount : (amt ? amt : 0),
@@ -23,6 +31,7 @@ angular.module('route').controller('tracker', function($scope) {
         category : cat});
   };
 
+// HANDLES THE ORDERBY VALUE OF THE TABLE
   $scope.selectOrder = function(col){
     if($scope.ordering == col){
       $scope.orderReverse = !$scope.orderReverse;
@@ -36,6 +45,7 @@ angular.module('route').controller('tracker', function($scope) {
     }
   };
 
+// CALCULATES RUNNING TOTAL OF ALL THE SELECTED COLUMNS
   $scope.calcTotal = function(){
     $scope.total = 0;
     for (var i = 0; i < $scope.transactions.length; i++) {//don't use for in otherwise it just returns indices
@@ -46,6 +56,33 @@ angular.module('route').controller('tracker', function($scope) {
     }
   }
   $scope.calcTotal();
+
+  $scope.selectAll = function(){
+    for(var i = 0; i < $scope.transactions.length; i++){
+      $scope.transactions[i].selected = true;
+    }
+    $scope.calcTotal();
+
+  }
+
+  $scope.deselectAll = function(){
+    for(var i = 0; i < $scope.transactions.length; i++){
+      $scope.transactions[i].selected = false;
+    }
+    $scope.calcTotal();
+
+  }
+
+  $scope.removeSelected = function(){
+    $scope.deletedTransactions.push($scope.transactions.filter( function(t){
+      return t.selected == true;
+    }));
+
+    $scope.transactions = $scope.transactions.filter( function(t){
+      return t.selected == false;
+    });
+    $scope.calcTotal();
+  }
 
 
 });
